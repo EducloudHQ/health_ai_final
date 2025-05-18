eb3b272fc3d64b311e7b808af2c4ca501e2f416c
trey-rosius <treyrosius@gmail.com>
2025-05-18 11:13:57 +0100

    Initial commit

---
+++ test/health_ai_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HealthAiCdk from '../lib/health_ai_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/health_ai_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HealthAiCdk.HealthAiCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
